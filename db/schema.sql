f1efe71045e1e24a534de1fcbc58bb7fed193308
Zack Greenfield <zackg007@verizon.net>
2022-02-13 13:40:02 -0500

    added to schema file and created index.js file for my queries

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
+
+
+
+
+CREATE TABLE department(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    NAME VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE role(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL UNIQUE,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
