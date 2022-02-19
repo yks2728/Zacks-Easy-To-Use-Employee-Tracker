6aad811e925de2c1fe43ef566178dd254ed329f5
Zack Greenfield <zackg007@verizon.net>
2022-02-19 10:20:19 -0500

    added express.js

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const inquire = require("inquirer");
+const employeeData = [];
+
+const addData = () => {
+  return inquire.prompt([
+    {
+      type: "choice",
+      name: "companyData",
+      message: "Choose From the Following Options",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+      ],
+    },
+    {},
+  ]);
+};
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const require = ("inquirer");
-const employeeData = [];
+const express = require("express");
+const path = require("path");
+const db = require("./db/connection.js");
 
-const addData = () => {
-    return (
-        inquire
-            .prompt([
-                {
-                    type: "choice",
-                    name: "companyData",
-                    message: "Choose From the Following Options",
-                    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-                },
-                {
-                    
-                }
-            ])
-    )
-}
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = db;
